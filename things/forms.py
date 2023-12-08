fc8ee65e8e941f077b8d28f05625c63a51d51755
Onyi Dozie <onyiyechukwu.dozie@kcl.ac.uk>
2023-12-08 15:37:27 +0000

    solution

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,17 @@
 """Forms of the project."""
+from .models import Thing
+from django import forms
 
 # Create your forms here.
+
+class ThingForm(forms.Form):
+    class Meta: 
+        model = Thing
+        exclude = ['created_at']
+        fields = ['name','description','quantity']
+        widgets = {
+            'description': forms.Textarea(),
+            'quantity': forms.NumberInput(attrs={'min':0}),
+        }
+
+
--- things/views.py
+++ things/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else: 
+        form = ThingForm()
+        
     return render(request, 'home.html', {'form': form})
